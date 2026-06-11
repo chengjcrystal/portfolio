9530bf164b3f3bce48f6b595765ca3dfa2c601e1
Crystal Cheng <chengjcrystal@gmail.com>
2026-06-11 15:26:52 -0700

    design edits

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,47 +1,22 @@
-"use client";
-
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 export default function Nav() {
-  const pathname = usePathname();
-
-  // smooth scroll for same-page hash links, normal nav otherwise
-  const handleHash = (e: React.MouseEvent<HTMLAnchorElement>, hash: string) => {
-    if (pathname === "/") {
-      e.preventDefault();
-      document.querySelector(hash)?.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
   return (
     <nav>
       <Link href="/" className="nav-logo">
         Crystal Cheng
       </Link>
       <ul className="nav-links">
+        <li><a href="/#experience">Experience</a></li>
+        <li><a href="/#projects">Projects</a></li>
+        <li><a href="/#skills">Skills</a></li>
         <li>
-          <a href="/#experience" onClick={(e) => handleHash(e, "#experience")}>
-            Experience
-          </a>
-        </li>
-        <li>
-          <a href="/#projects" onClick={(e) => handleHash(e, "#projects")}>
-            Projects
-          </a>
-        </li>
-        <li>
-          <a href="/#skills" onClick={(e) => handleHash(e, "#skills")}>
-            Skills
-          </a>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/contact" className="nav-contact">
+          <a
+            href="mailto:cjcheng@berkeley.edu"
+            className="nav-contact"
+          >
             Contact
-          </Link>
+          </a>
         </li>
       </ul>
     </nav>
